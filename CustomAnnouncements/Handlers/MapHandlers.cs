5a4286d4fe16de41c9404d578748dafa8a33cfb0
BuildBoy12 <buildboy12345@gmail.com>
2020-11-15 12:50:53 -0500

    ScpTermination Update
    Fix NRE, updated variables

--- CustomAnnouncements/Handlers/MapHandlers.cs
+++ CustomAnnouncements/Handlers/MapHandlers.cs
@@ -1,7 +1,11 @@
 namespace CustomAnnouncements.Handlers
 {
+    using Exiled.API.Features;
     using Exiled.Events.EventArgs;
+    using Respawning;
+    using Respawning.NamingRules;
     using System;
+    using System.Linq;
     using static CustomAnnouncements;
 
     public class MapHandlers
@@ -12,17 +16,56 @@ namespace CustomAnnouncements.Handlers
                 return;
 
             ev.IsAllowed = false;
-            string unitName = !Respawning.NamingRules.UnitNamingRules.TryGetNamingRule(
-                Respawning.SpawnableTeamType.NineTailedFox,
-                out var unitNamingRule)
-                ? "Unknown"
-                : "containmentunit " + unitNamingRule.GetCassieUnitName(ev.Killer.ReferenceHub.characterClassManager.CurUnitName);
 
+            string terminationCause;
+            var damageType = ev.HitInfo.GetDamageType();
+            if (damageType == DamageTypes.Tesla)
+            {
+                terminationCause = "SUCCESSFULLY TERMINATED BY AUTOMATIC SECURITY SYSTEM";
+            }
+            else if (damageType == DamageTypes.Nuke)
+            {
+                terminationCause = "SUCCESSFULLY TERMINATED BY ALPHA WARHEAD";
+            }
+            else if (damageType == DamageTypes.Decont)
+            {
+                terminationCause = "LOST IN DECONTAMINATION SEQUENCE";
+            }
+            else
+            {
+                if (ev.Killer != null)
+                {
+                    terminationCause = ev.Killer.Team switch
+                    {
+                        Team.MTF => !UnitNamingRules.TryGetNamingRule(SpawnableTeamType.NineTailedFox,
+                            out var unitNamingRule)
+                            ? "UNKNOWN"
+                            : $"CONTAINEDSUCCESSFULLY CONTAINMENTUNIT {unitNamingRule}",
+                        Team.CHI => "CONTAINEDSUCCESSFULLY BY CHAOSINSURGENCY",
+                        Team.RSC => "CONTAINEDSUCCESSFULLY BY SCIENCE PERSONNEL",
+                        Team.CDP => "CONTAINEDSUCCESSFULLY BY CLASSD PERSONNEL",
+                        _ => "SUCCESSFULLY TERMINATED . CONTAINMENTUNIT UNKNOWN"
+                    };
+                }
+                else
+                {
+                    terminationCause = "SUCCESSFULLY TERMINATED . TERMINATION CAUSE UNSPECIFIED";
+                }
+            }
+
+            var scpRole = ev.Role.fullName.Split('-')[1];
+            string announcedScpRole = "SCP ";
+            for (int i = 0; i < scpRole.Length; i++)
+            {
+                announcedScpRole += scpRole[i] + " ";
+            }
+
+            int scpsRemaining = Player.Get(Team.SCP).Count() - 1;
             var overrideMessage = Instance.Config.ScpTerminated.Message.ReplaceAfterToken('$', new[]
             {
-                new Tuple<string, object>("TerminationCause", ev.TerminationCause),
-                new Tuple<string, object>("TerminatorRole", ev.Role),
-                new Tuple<string, object>("NtfUnit", unitName)
+                new Tuple<string, object>("TerminationCause", terminationCause),
+                new Tuple<string, object>("KilledScp", announcedScpRole),
+                new Tuple<string, object>("RemainingScps", scpsRemaining),
             });
             Instance.Methods.PlayAnnouncement(Instance.Config.ScpTerminated, overrideMessage);
         }
